d9266ece1a8b627c9cd33cced83e4b2a1f2f6c51
atsufranc <atsufranc200363@gmail.com>
2025-09-07 23:49:01 +0000

    django-models

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -12,5 +12,5 @@ def books_in_library(library_name):
 
 # Retrieve the librarian for a library
 def librarian_for_library(library_name):
-    library = Library.objects.get(name=library_name)
+    library = Library.objects.get(library=library_name)
     return library.librarian
