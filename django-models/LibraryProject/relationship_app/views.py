9cf4a3d0d33b1e0bf7ef62d7c3a298040f679401
atsufranc <atsufranc200363@gmail.com>
2025-09-08 00:08:05 +0000

    django-models

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from django.views.generic import DetailView
 from .models import Book, Library
+from django.views.generic.detail import DetailView
 from .models import Library, Librarian, UserProfile 
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
