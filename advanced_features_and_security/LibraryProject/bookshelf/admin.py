228d44d7e00a634959d8799c9693ce94d9da24cb
atsufranc <atsufranc200363@gmail.com>
2025-09-14 21:16:43 +0000

    advance features and security

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -13,3 +13,33 @@ class BookAdmin(admin.ModelAdmin):
 
     # Add filters for publication year
     list_filter = ("publication_year",)
+
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import CustomUser, UserProfile, Author, Book, Library, Librarian
+
+class CustomUserAdmin(BaseUserAdmin):
+	model = CustomUser
+	list_display = ('email', 'first_name', 'last_name', 'is_staff', 'is_active', 'date_of_birth')
+	list_filter = ('is_staff', 'is_active', 'date_of_birth')
+	search_fields = ('email', 'first_name', 'last_name')
+	ordering = ('email',)
+	fieldsets = (
+		(None, {'fields': ('email', 'password')}),
+		('Personal info', {'fields': ('first_name', 'last_name', 'date_of_birth', 'profile_photo')}),
+		('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+		('Important dates', {'fields': ('last_login', 'date_joined')}),
+	)
+	add_fieldsets = (
+		(None, {
+			'classes': ('wide',),
+			'fields': ('email', 'password1', 'password2', 'first_name', 'last_name', 'date_of_birth', 'profile_photo', 'is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+		}),
+	)
+
+admin.site.register(CustomUser, CustomUserAdmin)
+# admin.site.register(UserProfile)
+# admin.site.register(Author)
+# admin.site.register(Book)
+# admin.site.register(Library)
+# admin.site.register(Librarian)
