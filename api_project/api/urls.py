c9fb604c88ce6aa98d7c859e089df07d99bb2b2f
atsufranc <atsufranc200363@gmail.com>
2025-09-28 21:56:19 +0000

    modify view to viewset

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,11 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 from .views import BookList, BookViewSet # Import the BookList view
 from rest_framework.routers import DefaultRouter    
 
 router = DefaultRouter()
-router.register(r'', BookViewSet, basename='book_all')
+router.register(r'book_all', BookViewSet, basename='book_all')
 
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),  # Maps to the BookList view
+    path('', include(router.urls)),  # Include the router URLs
 ]
-urlpatterns += router.urls
